2249dc7e928065c61eb28a25ce69cc60c673043d
rodisho <rafiodisho@gmail.com>
2023-09-19 00:49:10 -0400

    handle not found page

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -2,6 +2,9 @@ import Link from 'next/link';
 
 // Fetch tickets
 async function getTickets() {
+  // imitate delay
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
   const res = await fetch('http://localhost:4000/tickets', {
     next: {
       revalidate: 0,
