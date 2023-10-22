572f02852872fc1b192492261db7398b2a3c3335
rodisho <rafiodisho@gmail.com>
2023-10-22 16:54:13 -0400

    Implemented meta data for individual pages | page metadata

--- app/(dashboard)/tickets/page.jsx
+++ app/(dashboard)/tickets/page.jsx
@@ -2,6 +2,11 @@ import { Suspense } from 'react';
 import TicketList from './TicketList';
 import Loading from '../loading';
 
+
+export const metadata = {
+    title: 'Dojo Helpdesk | Tickets'
+};
+
 export default function Tickets() {
   return (
     <main>
