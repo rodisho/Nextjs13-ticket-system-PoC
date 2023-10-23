046a8c5f765ac1803524d018f31526e0b6190686
rodisho <rafiodisho@gmail.com>
2023-10-23 00:20:37 -0400

    implemented auth form using Supabase auth

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+"use client"
+
+import AuthForm from "../AuthForm";
 
 export default function Login() {
+
+    const handleSubmit = async(e, email, password) =>{
+        e.preventDefault()
+
+        console.log("User Login", email,  password);
+    }
+
+
     return (
         <main>
             <h2 className="text-center">Log in</h2>
+
+            <AuthForm handleSubmit={handleSubmit}/>
         </main>
     );
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,8 +1,19 @@
+"use client"
+
+import AuthForm from "../AuthForm";
 
 export default function Signup() {
+
+    const handleSubmit = async(e, email, password) => {
+        e.preventDefault()
+
+        console.log("User Signup", email,  password);
+    }
+
     return (
        <main>
            <h2 className="text-center">Sign up</h2>
+           <AuthForm handleSubmit={handleSubmit}/>
        </main>
     );
 }
