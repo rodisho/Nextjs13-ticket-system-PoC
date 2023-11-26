4fc230800cdd2767d2628a264029c0d68b53d6cd
rodisho <rafiodisho@gmail.com>
2023-11-25 21:44:21 -0500

    Implemented routes protections when user is logged out

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,12 +1,16 @@
 import Navbar from "@/app/components/Navbar";
 import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
 import {cookies} from 'next/headers'
+import {redirect} from "next/navigation";
 
 // components
 export default async function DashboardLayout({children}) {
     const supabase = createServerComponentClient({cookies})
     const { data } = await supabase.auth.getSession();
 
+    if(!data.session){
+        redirect('/login')
+    }
     return (
        <>
        <Navbar user={data.session.user}/>
